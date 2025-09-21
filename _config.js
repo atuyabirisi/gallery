d955513dac0707e15d7d562e187f88818dc7bf6b
atuyabirisi <tushhamphrey@gmail.com>
2025-09-21 20:56:16 +0300

    connect application to mongoDB Atlas

--- _config.js
+++ _config.js
@@ -1,9 +1,35 @@
-var config = {}
+const { MongoClient } = require("mongodb");
 
-// Update to have your correct username and password
-config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+async function connectDB() {
+  let uri;
+
+  switch (process.env.NODE_ENV) {
+    case "test":
+      uri = process.env.DB_URL_TEST;
+      break;
+    case "development":
+      uri = process.env.DB_URL_DEV;
+      break;
+    case "production":
+      uri = process.env.DB_URL_PROD;
+      break;
+    default:
+      uri = process.env.DB_URL_DEV;
+  }
+
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  try {
+    await client.connect();
+
+    await client.db("darkroom").command({ ping: 1 });
+    console.log("connected to MongoDB!");
+  } catch (error) {
+    console.error(error);
+  }
 }
-module.exports = config;
+
+module.exports = connectDB;
